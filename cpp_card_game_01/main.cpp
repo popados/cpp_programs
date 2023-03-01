091156593c68a8518fad7f3d6c8ba3c1438b3ef8
popados <popados@gmail.com>
2023-02-28 23:42:41 -0600

    fixed loop function

--- cpp_card_game_01/main.cpp
+++ cpp_card_game_01/main.cpp
@@ -21,7 +21,8 @@ class CardCreator {
         bool isCastable;
 
     public:
-        char name[16];
+    //change to char
+        string name;
         int hp;
         int atk;
         int mana;
@@ -37,9 +38,48 @@ class CardCreator {
                 // MyFile.close();
                 return false;
             }
-
-
         };
+
+
+// methods go here
+
+        string getCardName () {
+            cout << "enter card name: " << endl;
+                // getline(cin,name);
+                // // cin >> name;
+                //cin.getline(name, 20) >> ws;
+                // cout << "the card is called: " << name << endl;
+        //this fix came from [https://stackoverflow.com/questions/26071275/c-while-loop-and-getline-issue]
+        //must clear the buffer for the while loop to allow input(break the loop)
+            //getline(cin, name);
+            //cin.ignore(256,'\n');
+            getline(cin >> ws, name);
+            cout << "The name is: " << name << endl;
+            return name;
+            }
+
+        int getCardAtk(){               
+            
+                cout << "enter card atk value: " ;
+                cin >> atk;
+                cout << "the card atk value: " << atk << endl;
+                return atk;
+            }
+
+        int getCardHealth(){
+                cout << "enter card health value: " ;
+                cin >> hp;
+                cout << "the card hp value: " << hp << endl;
+                return hp;
+            }
+
+
+        int getCardMana(){
+                cout << "enter card mana value: " ;
+                cin >> mana;
+                cout << "the card mana value: " << mana << endl;
+                return mana;
+            }
 
         int CardsToCreate() {
             cout << "enter number of cards to create: ";
@@ -51,7 +91,62 @@ class CardCreator {
 
         }
 
+        void saveCard(atk,hp,mana,name) {
+            MyFile << "{" << "\"" << "name" << "\"" << ":\"" << name << "\",\"" << "Attack"  << "\"" << ":\"" << atk_pow << "\"}" ;
+
+            int userInput;
+            cin >> userInput;
+
+            switch(userInput) {
+                case 1:
+                cout << "continue" << endl;
+                MyFile << "," << endl;
+                 break;
+                case 2: 
+                cout << "end" << endl;
+                MyFile << "]" << endl;
+                 break;
+                default:
+                    cout << "default end" << endl;
+                    MyFile << "]" << endl;       
+                    break;
+            }
+
 };
+
+
+void cardCreationInput () {
+    CardCreator machine;
+    machine.cards_to_create = 0;
+    machine.name;
+    cout << "is this random? " << machine.cards_to_create << endl;
+    cout << "cards left to create: " << machine.cards_to_create << endl;
+    machine.CardsToCreate();
+
+    for (int i = 0; i < machine.cards_to_create; i++) {
+        cout << "cards to create: " << machine.cards_to_create << endl;
+        machine.getCardName();
+        machine.getCardAtk();
+        machine.getCardHealth();
+        machine.getCardMana();
+    }
+
+    // while(machine.cards_to_create > 0) {
+    //    
+    //     machine.getCardName();
+        
+    //     //break;
+    //     machine.cards_to_create--;
+    //     //save card and do json things
+    //     //create cards
+    //     //ask name, atk, hp, mana, iscreature, iscastable.
+
+
+    // }    
+    
+}
+
+
 
 void userStartSelection (CardCreator machine) {
     cout << "enter 1 for new file, 2 for existing file, or 3 . any other key will exit." << endl; 
@@ -59,10 +154,11 @@ void userStartSelection (CardCreator machine) {
     cin >> userInput;
     CardCreator machine1;
 
+
     switch(userInput) {
         case 1:
             cout << "pressed 1" << endl;
-            cout << "cards left: " << machine1.cards_to_create << endl;
+            cardCreationInput();
             //return machine1.cards_to_create;
             break;
         case 2:
@@ -79,30 +175,18 @@ void userStartSelection (CardCreator machine) {
     }
 }
 
-void cardCreationInput () {
 
-    CardCreator machine;
-    machine.fileCheck();
-    machine.cards_to_create = 0;
-    userStartSelection(machine);
-    cout << "is this random? " << machine.cards_to_create << endl;
-    machine.CardsToCreate();
-    while(machine.cards_to_create > 0) {
-        machine.cards_to_create--;
-        cout << "cards to create: " << machine.cards_to_create << endl;
-        //create cards
-        //ask name, atk, hp, mana, iscreature, iscastable.
-    }
-    
-    
-    
-    }
 
 int main(){
     std::cout << "hello world" << std::endl;
     myFunction();
-    cardCreationInput();
-
+    CardCreator machine;
+    machine.fileCheck();
+    machine.cards_to_create = 0;
+    userStartSelection(machine);
+    // machine.name;
+    // getline(cin, machine.name);
+    // cout << machine.name;
    
 
 
